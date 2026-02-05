dc1afd7d75321364637379ed0c694ca10a1e7d2d
maxence30 <henous.maxence@outlook.fr>
2026-02-05 09:25:59 +0000

    ok

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-"""Function that checks if an object inherits from a class (but not exact class)."""
+"""
+Function that checks if an object is an instance of a class that inherited
+(directly or indirectly) from the specified class.
+"""
 
 
 def inherits_from(obj, a_class):
-    """Return True if obj is an instance of a subclass of a_class."""
+    """
+    Return True if obj is an instance of a subclass of a_class,
+    otherwise False.
+    """
     return issubclass(type(obj), a_class) and type(obj) is not a_class
