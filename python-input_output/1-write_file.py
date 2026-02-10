dde99d2420d27165989b32ba324293c4ee78a25e
maxence30 <henous.maxence@outlook.fr>
2026-02-10 08:32:11 +0000

    z

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,6 +4,8 @@
 
 
 def write_file(filename="", text=""):
-    """Écrit du texte dans un fichier UTF-8 et retourne le nombre de caractères écrits."""
+    """Écrit du texte dans un fichier UTF-8 et retourne le nombre
+    de caractères écrits.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
