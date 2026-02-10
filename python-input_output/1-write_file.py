178786b5a519cd03d260d65235052a3b6da88fc1
maxence30 <henous.maxence@outlook.fr>
2026-02-10 08:31:27 +0000

    lo

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module qui écrit une chaîne de caractères dans un fichier texte UTF-8.
+"""
+
+
+def write_file(filename="", text=""):
+    """Écrit du texte dans un fichier UTF-8 et retourne le nombre de caractères écrits."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
