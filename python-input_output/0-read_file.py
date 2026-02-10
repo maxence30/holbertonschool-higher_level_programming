8ced0b7a8009551fb8a4c827fa985702384e8a9e
maxence30 <henous.maxence@outlook.fr>
2026-02-10 08:29:59 +0000

    mp

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,5 +2,6 @@
 """Module qui lit un fichier texte UTF-8 et l'affiche sur la sortie standard."""
 
 def read_file(filename=""):
+    """Lit un fichier texte UTF-8 et affiche son contenu sur stdout."""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
