ffb267021e8651d4e5f5bf500655c0fa2453afeb
maxence30 <henous.maxence@outlook.fr>
2026-06-30 15:01:41 +0200

    task 1 to 5 project JavaScript Warm-up

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.arg.length - 2;
+
+if(args === 0){
+    console.log('No argument');
+} else if (args === 1){
+    console.log('Argument found');
+} else {
+    console.log ('Arguments found')
+}
